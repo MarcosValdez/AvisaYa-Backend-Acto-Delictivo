0a4747669e900d4efa59004157aa9656166e4511
MarcosValdez <marcos.valdez333@hotmail.com>
2022-11-18 18:43:53 -0500

    configuracion test, se agrego archivo user.spec.js

--- test/user.spec.js
+++ test/user.spec.js
@@ -1,4 +1,3 @@
-import app from '../index.js'
 import chai from 'chai'
 import request from 'request'
 
@@ -7,9 +6,9 @@ const expect = chai.expect
 const should = chai.should()
 
 const url = "http://localhost:8080";
-describe('Testing assert function: ', function() {
+describe('Testing user enpoint: ', function() {
     describe('Check addtest funtion', function(){
-        it("metodo prueba 2 GET", async() => {
+        it("metodo prueba user 2 GET", async() => {
             let entpoint = url+"/user/prueba"
             request(entpoint, function(error, response, body) {
                 expect(response.statusCode).to.equal(200);
